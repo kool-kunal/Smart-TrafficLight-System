1a24105cfaa555c882a9ded42ffbaaf275a57872
kool-kunal <kunaltaneja28@gmail.com>
2022-04-28 14:13:06 +0530

    saved route files

--- multi-intersection/main.py
+++ multi-intersection/main.py
@@ -11,11 +11,11 @@ N_CARS = 400
 NUM_STATES = 40
 SUMO_CONFIG_FILE_NAME = 'sumo_config.sumocfg'
 NEAT_CONFIG_FILE_PATH = 'config/config-feedforward.txt'
-TEST_MODEL_PATH = 'C:/Users/karti/Desktop/New_Training/Smart-TrafficLight-System/checkpoints/training_with_avg_waiting_time_only/winner_1110.p'
+TEST_MODEL_PATH = '/home/kunal/Desktop/college/major_project/Smart-TrafficLight-System/winner_1110.p'
 GREEN_DURATION = 15
 YELLOW_DURATION = 3
-TEST_RUNS = 100
-GUI = False
+TEST_RUNS = 1
+GUI = True
 
 
 def run_test(net):
@@ -39,6 +39,15 @@ def allocate_new_dir(folder_name):
     return new_dir_path
 
 
+def print_result(results):
+    print("{:<15} {:<8} {:<10} {:<10} {:<8} {:<8}".format(
+        'Test run', 'Model', 'RMS_LOSS', 'HM_LOSS', 'AQL', 'AWT'))
+    print("------------------------------------------------------------")
+    for i in range(len(results)):
+        print(f"{i+1:<15} {'net':<8} {results[i+1]['net']['RMS_WAITING_TIME_LOSS']:<10.3f} {results[i+1]['net']['HARMONIC_MEAN_LOSS']:<10.3f} {results[i+1]['net']['AVERAGE QUEUE LENGTH']:<8.3f} {results[i+1]['net']['AVERAGE WAITING TIME']:<8.3f}")
+        print(f"{i+1:<15} {'ttl':<8} {results[i+1]['ttl']['RMS_WAITING_TIME_LOSS']:<10.3f} {results[i+1]['ttl']['HARMONIC_MEAN_LOSS']:<10.3f} {results[i+1]['ttl']['AVERAGE QUEUE LENGTH']:<8.3f} {results[i+1]['ttl']['AVERAGE WAITING TIME']:<8.3f}")
+
+
 if __name__ == "__main__":
 
     genome = pickle.load(open(
@@ -50,7 +59,7 @@ if __name__ == "__main__":
     net = neat.nn.FeedForwardNetworknet = neat.nn.feed_forward.FeedForwardNetwork.create(
         genome, config)
 
-    generator = TrafficGenerator(MAX_STEPS, N_CARS)
+    #generator = TrafficGenerator(MAX_STEPS, N_CARS)
 
     results = {}
 
@@ -58,13 +67,15 @@ if __name__ == "__main__":
                   'AVERAGE_QUEUE_LENGTH', 'AVERAGE_WAITING_TIME']
 
     for i in range(TEST_RUNS):
-        generator.generate_routefile(i)
+        #generator.generate_routefile(i)
         result = run_test(net)
         results[i+1] = result
 
-    new_dir_path = allocate_new_dir('car_400_ttl_60')
+    print('\n\n')
+    print_result(results)
+    # new_dir_path = allocate_new_dir('car_400_ttl_60')
 
-    with open(new_dir_path + "/test_results.json", "w") as test:
-        json.dump(results, test, indent=4)
+    # with open(new_dir_path + "/test_results.json", "w") as test:
+    #     json.dump(results, test, indent=4)
 
-    generate_test_plots(new_dir_path)
+    # generate_test_plots(new_dir_path)
